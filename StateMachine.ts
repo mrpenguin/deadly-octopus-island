407113c9d9067e8a2e77ff24cf7788295f5fa78c
mrpenguin <sadcebu2003@yahoo.com>
2020-03-19 08:54:05 -0500

    Setting up interface for changing state

--- StateMachine.ts
+++ StateMachine.ts
@@ -3,7 +3,7 @@ namespace StateMachine{
     export interface IStateMachine{
         onStateChange(): (currentState:string) => void
         onBeforeStateChange(): (newState:string, oldState:string) => void
-
+        changeState(newState:string): void
     }
     export class StateMachine {
 
